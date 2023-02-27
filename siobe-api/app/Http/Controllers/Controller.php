a8231245d8cf4fc213b47207ed592500ec4e6a92
cleavor <86359265+cleavor@users.noreply.github.com>
2023-02-27 09:46:24 +0700

    up1

--- siobe-api/app/Http/Controllers/Controller.php
+++ siobe-api/app/Http/Controllers/Controller.php
@@ -5,8 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\User;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function getOneUser($id)
+{
+    $users = User::find($id);
+    return response()->json([
+    'status' => 'Success',
+    'message' => 'all users grabbed',
+    'data' => [
+    'users' => $users,
+    ]
+    ],200);
+    }
 }
+
--- siobe-api/routes/web.php
+++ siobe-api/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
+ 
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/{id}', [Controller::class, 'getOneUser']);
+
