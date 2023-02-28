a049915fcf60d1455873efb36064754019e51733
lintang pramesti <lintangpramesti77@gmail.com>
2023-02-28 21:30:00 +0700

    \app\Http\Controllers\FacultyController.php

--- siobe-api/routes/api.php
+++ siobe-api/routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('fakultas', [\App\Http\Controllers\FacultyController::class, 'index']);
