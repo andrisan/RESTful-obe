a8951a3670d2c1da4c77e23d14163aa3a81b9de6
ameliakw22 <ameliakw22@gmail.com>
2023-03-02 12:21:33 +0700

    fix userController

--- siobe-api/app/Http/Controllers/UserController.php
+++ siobe-api/app/Http/Controllers/UserController.php
@@ -10,12 +10,6 @@ class UserController extends Controller
     /**
      * Show the profile for a given user.
      */
-    public function show(string $id): View
-    {
-        return view('user.profile', [
-            'user' => User::findOrFail($id)
-        ]);
-    }
 
     public function getUser()
     {
