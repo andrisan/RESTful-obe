b8371e5e020e71cf6ed9a6a338376d42247e91a9
Daffa Raka <daffarakals@gmail.com>
2023-03-16 17:00:19 +0700

    Faculty Controller

--- siobe-api/app/Http/Controllers/FacultyController.php
+++ siobe-api/app/Http/Controllers/FacultyController.php
@@ -2,63 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Faculty;
+use App\Filters\UsersFilter;
 use Illuminate\Http\Request;
+use App\Filters\FacultyFilter;
+use App\Http\Requests\FacultyStoreRequest;
+use App\Http\Requests\FacultyUpdateRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Resources\UserCollection;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\FacultyResource;
+use App\Http\Resources\FacultyCollection;
 
 class FacultyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request): FacultyCollection
     {
-        //
+        $filter = new FacultyFilter();
+        $filterItems = $filter->filter($request);
+
+        $faculty = Faculty::where($filterItems)->paginate(10);
+        return new FacultyCollection($faculty->appends(request()->query()));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function store(FacultyStoreRequest $request): FacultyResource
     {
-        //
+        $validated = $request->validated();
+
+        return new FacultyResource(Faculty::create($validated));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function show(Faculty $Faculty): FacultyResource
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return new FacultyResource($Faculty);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(FacultyUpdateRequest $request, Faculty $Faculty): FacultyResource|JsonResponse
     {
-        //
+        $validated = $request->validated();
+        if (empty($validated)) {
+            return response()->json(['message' => 'Not modified'], 304);
+        }
+
+        $Faculty->update($validated);
+        return new FacultyResource($Faculty);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Faculty $Faculty): JsonResponse
     {
-        //
+        $Faculty->delete();
+        return response()->json(['message' => 'Resource deleted']);
     }
 }
