05e307872346107fd0fcbccb3916830f1afb492e
catherinevania <catherinevania@student.ub.ac.id>
2023-03-16 14:35:06 +0700

    add FacultyStoreRequest and FacultyUpdateRequest

---
+++ siobe-api/app/Http/Resources/FacultyResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FacultyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+			$includeDate = $request->query('includeDate');
+			return [
+					'name'=> $this->name,
+					$this->mergeWhen($includeDate, [
+							'email_verified_at' => $this->email_verified_at,
+							'created_at' => $this->created_at,
+							'updated_at' => $this->updated_at,
+					]),
+			];
+    }
+}
--- siobe-api/routes/api.php
+++ siobe-api/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
