55dc1cc1184f9a37a01b24669e52185fa9826413
Dhronz <42285465+Dhronz@users.noreply.github.com>
2023-04-06 14:16:19 +0700

    Add files via upload

--- siobe-api/routes/api.php
+++ siobe-api/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\CriteriaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -20,15 +19,32 @@ use App\Http\Controllers\SyllabusController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('study-programs',[StudyProgramController::class, 'index'])->name('study-programs.get');
+Route::post('study-programs',[StudyProgramController::class, 'store'])->name('study-programs.post');
+Route::put('study-programs/{study_programs}', [StudyProgramController::class, 'update'])->name('study-programs.update');
+Route::delete('study-programs/{study_programs}', [StudyProgramController::class, 'destroy'])->name('study-programs.destroy');
+
+//StudyProgram Routes
+Route::get('study-programs',[StudyProgramController::class, 'index'])->name('study-programs.index');
+Route::post('study-programs',[StudyProgramController::class, 'store'])->name('study-programs.store');
+Route::patch('study-programs/{study_program}', [StudyProgramController::class, 'update'])->name('study-programs.update');
+Route::delete('study-programs/{study_program}', [StudyProgramController::class, 'destroy'])->name('study-programs.destroy');
+
+//Course Routes
+Route::get('courses',[CourseController::class, 'index'])->name('courses.index');
+Route::get('courses/create',[CourseController::class, 'create'])->name('courses.create');
+Route::post('courses',[CourseController::class, 'store'])->name('courses.store');
+Route::get('courses/{course}',[CourseController::class, 'edit'])->name('courses.edit');
+Route::patch('courses/{course}', [CourseController::class, 'update'])->name('courses.update');
+Route::delete('courses/{course}', [CourseController::class, 'destroy'])->name('study-programs.destroy');
+
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-     Route::apiResource('users', UserController::class);
- });
-=======
+   Route::apiResource('users', UserController::class);
+
     Route::apiResource('users', UserController::class);
     Route::apiResource('faculties',FacultyController::class);
     Route::apiResource('rubrics', RubricController::class);
-    Route::apiResource('rubrics.criterias', CriteriaController::class);
     
     Route::scopeBindings()->group(function () {
         Route::apiResource('faculties.departments', DepartmentController::class);
@@ -50,3 +66,4 @@ Route::post('register-device', function (Request $request) {
         'message' => 'Device registered successfully.',
         'token' => $token
     ]);
+});
