deb9405c8d7521a9494b4553f4610c56e3668de5
asyafaris15 <asyafaris15@student.ub.ac.id>
2023-04-06 14:56:37 +0700

    update & destroy fix

--- siobe-api/app/Http/Controllers/LessonLearningOutcomeController.php
+++ siobe-api/app/Http/Controllers/LessonLearningOutcomeController.php
@@ -5,11 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LessonLearningOutcomeStoreRequest;
 use App\Http\Requests\LessonLearningOutcomeUpdateRequest;
 use App\Http\Resources\LessonLearningOutcomeResource;
-use App\Models\Faculty;
 use App\Models\LessonLearningOutcome;
-use App\Models\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 
 class LessonLearningOutcomeController extends Controller
@@ -45,39 +42,24 @@ class LessonLearningOutcomeController extends Controller
 	 * Update the specified resource in storage.
 	 */
 	public function update(LessonLearningOutcomeUpdateRequest $request, LessonLearningOutcome $llo): LessonLearningOutcomeResource|JsonResponse
-<<<<<<< HEAD
-=======
 	{
->>>>>>> ff0b8a496d4acffbc31cf54c24df3ade3e0955ae
 		$validated = $request->validated();
         if (empty($validated)) {
             return response()->json(['message' => 'Not modified'], 304);
         }
 
-<<<<<<< HEAD
-        $faculty->update($validated);
-        return new LessonLearningResource($llo);
-=======
 		$llo->update($request->all());
 		return new LessonLearningOutcomeResource($llo);
->>>>>>> ff0b8a496d4acffbc31cf54c24df3ade3e0955ae
 	}
 
 	/**
 	 * Remove the specified resource from storage.
 	 */
-<<<<<<< HEAD
-	public function destroy(LessonLearningOutcome $llo): JsonResponse
-	{
-		$llo->delete();
-        return response()->json(['message' => 'Resource deleted']);
-=======
 	public function destroy(LessonLearningOutcome $llo)
 	{
         $llo->delete();
 
         return response()->json(['message' => 'Resource deleted']);
 
->>>>>>> ff0b8a496d4acffbc31cf54c24df3ade3e0955ae
 	}
 }
