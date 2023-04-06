a42be73f0d2f5d8f39154eab5b9052d35e39125b
asyafaris15 <asyafaris15@student.ub.ac.id>
2023-04-06 16:31:28 +0700

    Coba lagi

--- siobe-api/app/Http/Requests/LessonLearningOutcomeUpdateRequest.php
+++ siobe-api/app/Http/Requests/LessonLearningOutcomeUpdateRequest.php
@@ -6,38 +6,38 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LessonLearningOutcomeUpdateRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 */
+	public function authorize(): bool
+	{
+		return false;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
-     */
-    public function rules(): array
-    {
-        $method = $this->method();
-        if ($method == 'PATCH') {
-            return [
-                'clo_id' => ['required', 'integer'],
-                'position' => ['sometimes','integer'],
-                'description' => ['sometimes','string'],
-                'code' => ['required', 'string'],
-                'syllabus_id' => ['required','integer']
-            ];
-        } else {
-            return [
-                'clo_id' => ['required', 'integer'],
-                'position' => ['required','integer'],
-                'description' => ['required','string'],
-                'code' => ['required', 'string'],
-                'syllabus_id' => ['required','integer']
-            ];
- }
-}
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+	 */
+	public function rules(): array
+	{
+		$method = $this->method();
+		if ($method == 'PATCH') {
+			return [
+				'clo_id' => ['required', 'integer'],
+				'position' => ['sometimes', 'integer'],
+				'description' => ['sometimes', 'string'],
+				'code' => ['required', 'string'],
+				'syllabus_id' => ['required', 'integer']
+			];
+		} else {
+			return [
+				'clo_id' => ['required', 'integer'],
+				'position' => ['required', 'integer'],
+				'description' => ['required', 'string'],
+				'code' => ['required', 'string'],
+				'syllabus_id' => ['required', 'integer']
+			];
+		}
+	}
 }
