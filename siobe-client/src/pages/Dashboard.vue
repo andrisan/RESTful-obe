7771bf402d6fd719e1e3f5816a23359a63634e3d
Dhronz <42285465+Dhronz@users.noreply.github.com>
2023-06-26 20:19:39 +0700

    Update Dashboard.vue

--- siobe-client/src/pages/Dashboard.vue
+++ siobe-client/src/pages/Dashboard.vue
@@ -6,101 +6,18 @@ import AuthenticatedLayout from '@/layouts/AuthenticatedLayout.vue'
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                OBE Kelompok 4
+                Dashboard
             </h2>
         </template>
 
-        <div class="py-4">
+        <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                
-                <!-- Navigate Button Page -->
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-2 bg-white border-b border-gray-200">
-                        <router-link
-                                v-slot="{ href, isActive, navigate }"
-                                to="/studyprograms"
-                                custom>
-                        <NavLink
-                                :href="href"
-                                    :active="isActive"
-                                    @click="navigate">
-                            <button type="button" class="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 mr-2 mb-2">
-                                    <svg class="w-4 h-4 mr-2 -ml-1 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
-                                    STUDY PROGRAM
-                                </button>
-                            </NavLink>
-                        </router-link>
-                    </div>
-
-                    <div class="p-2 bg-white border-b border-gray-200">
-                        <router-link
-                                v-slot="{ href, isActive, navigate }"
-                                to="/courses"
-                                custom>
-                        <NavLink
-                                :href="href"
-                                    :active="isActive"
-                                    @click="navigate">
-                            <button type="button" class="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 mr-2 mb-2">
-                                    <svg class="w-4 h-4 mr-2 -ml-1 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
-                                    COURSE
-                                </button>
-                            </NavLink>
-                        </router-link>
-                    </div>
-
-                    <div class="p-2 bg-white border-b border-gray-200">
-                        <router-link
-                                v-slot="{ href, isActive, navigate }"
-                                to="/courseclasses"
-                                custom>
-                        <NavLink
-                                :href="href"
-                                    :active="isActive"
-                                    @click="navigate">
-                            <button type="button" class="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 mr-2 mb-2">
-                                    <svg class="w-4 h-4 mr-2 -ml-1 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
-                                    COURSE CLASS
-                                </button>
-                            </NavLink>
-                        </router-link>
-                    </div>
-
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ml-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                                {{ store.userData.name }}
-
-                                                <svg
-                                                    class="ml-2 -mr-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor">
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd" />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink @click="submitLogout">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>                         
-                        </div>
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        Bagian ini yang dikerjakan oleh masing-masing kelompok
                     </div>
                 </div>
             </div>
+        </div>
     </AuthenticatedLayout>
 </template>
